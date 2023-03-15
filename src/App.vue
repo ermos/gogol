04ed54de172ca28dd585fbd456305b8df444518c
Kilian SMITI <kilian@smiti.fr>
2023-03-15 11:43:58 +0100

    feat: base template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
+import SearchBar from "./Components/SearchBar.vue";
+import SubmitBtn from "./Components/SubmitBtn.vue";
 </script>
 
 <template>
-  <div></div>
+  <div class="header">
+    <img src="./assets/images/logo.svg" />
+  </div>
+  <search-bar />
+  <center>
+
+    <submit-btn value="Recherche Google" aria-label="Recherche Google" />
+    <submit-btn value="J'ai de la chance" aria-label="J'ai de la chance" />
+
+    </center>
 </template>
 
 <style lang="scss">
+#app {
+  height: 100%;
+}
+
+.header {
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  height: calc(100% - 560px);
+}
 </style>
---
+++ src/Components/SearchBar.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+import SearchIcon from "./SearchIcon.vue";
+import MicIcon from "./MicIcon.vue";
+import CamIcon from "./CamIcon.vue";
+</script>
+
+<template>
+  <div class="searchbar">
+    <search-icon />
+    <input type="text" />
+    <mic-icon />
+    <cam-icon />
+  </div>
+</template>
+
+<style lang="scss">
+.searchbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 3;
+  height: 44px;
+  background: #fff;
+  border: 1px solid #dfe1e5;
+  box-shadow: none;
+  border-radius: 24px;
+  margin: 20px auto 0;
+  padding: 10px 15px;
+  width: auto;
+  max-width: 584px;
+
+  &:hover {
+    background-color: #fff;
+    box-shadow: 0 1px 6px rgba(32,33,36,.28);
+    border-color: rgba(223,225,229,0);
+  }
+
+  input {
+    background-color: transparent;
+    border: none;
+    padding: 0;
+    color: rgba(0,0,0,.87);
+    word-wrap: break-word;
+    outline: none;
+    display: flex;
+    -webkit-tap-highlight-color: transparent;
+    flex: 1;
+  }
+}
+</style>
